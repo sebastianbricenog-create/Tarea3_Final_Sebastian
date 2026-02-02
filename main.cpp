2b4bea92d91d7b9bb1e9cf4bc68462da31949570
sebastianbricenog-create <sebastian.briceno.g@utec.edu.pe>
2026-02-02 15:41:04 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -88,25 +88,34 @@ public:
             return;
         }
 
+        list<pair<string, list<string>>>& steps = it_macro->second;
+
+        for (list<pair<string, list<string>>>::iterator it_val = steps.begin(); it_val != steps.end(); ++it_val) {
+            if (comandos.find(it_val->first) == comandos.end()) {
+                cout << "!! Macro abortada: Comando '" << it_val->first << "' no registrado." << endl;
+                return;
+            }
+        }
+
         cout << "\n> Ejecutando Macro: " << name << endl;
         try {
-            for (auto& step : it_macro->second) {
-                execute(step.first, step.second);
+            for (list<pair<string, list<string>>>::iterator it_step = steps.begin(); it_step != steps.end(); ++it_step) {
+                execute(it_step->first, it_step->second);
             }
         } catch (...) {
-            cout << "!! Macro interrumpida por error de validacion." << endl;
+            cout << "!! Macro interrumpida." << endl;
         }
     }
 
     void mostrar_historial() {
         cout << "\n--- HISTORIAL DE IMPACTO ---" << endl;
-        for (auto it = historial.begin(); it != historial.end(); ++it)
-            cout << "* " << *it << endl;
+        for (list<string>::iterator it_h = historial.begin(); it_h != historial.end(); ++it_h)
+            cout << "* " << *it_h << endl;
     }
 };
 
 void f_curar(Entity& e, const list<string>& args) {
-    if (args.empty()) throw runtime_error("Sin argumentos");
+    if (args.empty()) throw runtime_error("Faltan argumentos");
     e.curar(stoi(args.front()));
 }
 void f_status(Entity& e, const list<string>& args) { cout << "[INFO] " << e.get_status() << endl; }
@@ -127,7 +136,7 @@ struct EnergyFunctor {
     }
 };
 struct SysLog {
-    void operator()(const list<string>& args) { cout << "[LOG] Operacion realizada." << endl; }
+    void operator()(const list<string>& args) { cout << "[LOG] Operacion confirmada." << endl; }
 };
 
 int main() {
@@ -144,25 +153,32 @@ int main() {
 
     center.registerCommand("full_hp", [&](const list<string>& a){ sebastian.curar(100); });
     center.registerCommand("poison", [&](const list<string>& a){ sebastian.danar(10); });
-    center.registerCommand("msg", [&](const list<string>& a){ cout << "Sistema activo" << endl; });
+    center.registerCommand("msg", [&](const list<string>& a){ cout << "Alerta de sistema" << endl; });
 
-    cout << "--- PRUEBAS DE FUNCIONAMIENTO ---" << endl;
-    center.execute("damage", {"15"});
+    center.execute("damage", {"20"});
+    center.execute("status", {});
+    center.execute("heal", {"10"});
+    center.execute("full_hp", {});
 
-    try {
-        center.execute("invalid_cmd", {});
-    } catch(const exception& e) { cout << "Validacion: " << e.what() << endl; }
-
-    try {
-        center.execute("heal", {"-10"});
-    } catch(const exception& e) { cout << "Validacion: " << e.what() << endl; }
+    try { center.execute("error_cmd", {}); } catch(const exception& e) {}
+    try { center.execute("damage", {}); } catch(const exception& e) {}
+    try { center.execute("heal", {"-50"}); } catch(const exception& e) {}
 
     center.removeCommand("burn");
+    try { center.execute("burn", {"10"}); } catch(const exception& e) {}
 
-    center.registerMacro("recovery_seq", { {"heal", {"20"}}, {"status", {}}, {"log", {}} });
-    center.executeMacro("recovery_seq");
+    center.registerMacro("m1", { {"heal", {"5"}}, {"invalid", {}} });
+    center.executeMacro("m1");
+    center.executeMacro("m_none");
+
+    center.registerMacro("prepa", { {"reset", {}}, {"msg", {}} });
+    center.registerMacro("fight", { {"damage", {"15"}}, {"poison", {}}, {"status", {}} });
+    center.registerMacro("recovery", { {"heal", {"25"}}, {"log", {}} });
+
+    center.executeMacro("prepa");
+    center.executeMacro("fight");
+    center.executeMacro("recovery");
 
     center.mostrar_historial();
-
     return 0;
 }
